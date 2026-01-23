49f47d54cf06c4a0231a7554d0169d2521e965ff
PatelDipen <dipen.d.patel@accenture.com>
2026-01-23 21:24:16 +0200

    Updated few dependencies version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.compose_app_sample_1"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.compose_app_sample_1"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
